3370b3936593bd77fa5c0e8b9eb7f2ee86d13bde
Kankana Bordoloi <kankana.bordoloi@gmail.com>
2025-09-10 22:40:20 +0530

    Update path_settings.py

--- common_utilities/path_settings.py
+++ common_utilities/path_settings.py
@@ -10,12 +10,12 @@ class PathSettings:
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
     if os.environ.get("CI") == "true":
-        DOWNLOAD_PATH = Path("/home/runner/work/dimagi-sa/dimagi-sa")
+        DOWNLOAD_PATH = Path("/home/runner/work/dimagi-qa-sureadhere/dimagi-sa")
     else:
         DOWNLOAD_PATH = Path('~/Downloads').expanduser()
 
     if os.environ.get("CI") == "true":
-        ROOT = os.path.abspath(os.pardir) + "/dimagi-sa"
+        ROOT = os.path.abspath(os.pardir) + "/dimagi-qa-sureadhere"
     else:
         ROOT = os.path.abspath(os.pardir)
 
