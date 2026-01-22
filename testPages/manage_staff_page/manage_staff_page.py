687bef651f4a15e1321edd0404f2a8b2f89be81e
Kankana Bordoloi <kankana.bordoloi@gmail.com>
2026-01-22 19:17:47 +0530

    QA-8323 increased load time for staff tabs

--- testPages/manage_staff_page/manage_staff_page.py
+++ testPages/manage_staff_page/manage_staff_page.py
@@ -76,7 +76,7 @@ class ManageStaffPage(BasePage):
 
     def open_inactive_tab(self):
         self.click("li_span_Inactive_tab")
-        time.sleep(5)
+        time.sleep(15)
         self.wait_for_page_to_load()
         self.wait_for_element("span_Manage_staff", 50)
         self.wait_for_element("tbody_staff", 100)
@@ -84,7 +84,7 @@ class ManageStaffPage(BasePage):
 
     def open_test_tab(self):
         self.click("li_span_Test_tab")
-        time.sleep(5)
+        time.sleep(15)
         self.wait_for_page_to_load()
         self.wait_for_element("span_Manage_staff", 50)
         self.wait_for_element("tbody_staff", 100)
