ff689f62e9e014cbfd93cd7472d92e7abfbc6fc5
kbo001 <kbordoloi@dimagi.com>
2026-07-25 18:40:24 +0530

    QA-8581 changes reverted to old push

--- testPages/patient_tab_pages/patient_video_page.py
+++ testPages/patient_tab_pages/patient_video_page.py
@@ -138,7 +138,7 @@ class PatientVideoPage(BasePage):
         self.select_dose_status("Taken")
         side_effect = self.fill_up_side_effects()
         self.scroll_to_element('span_SUBMIT_REVIEW')
-        self.click_rendered('span_SUBMIT_REVIEW')
+        self.click('span_SUBMIT_REVIEW', strict=True)
         time.sleep(2)
         try:
             self.kendo_dialog_wait_open()  # no title constraint
